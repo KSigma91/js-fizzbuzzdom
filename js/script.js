e6168bb8afcc3349e524a910e0de63282af52ca1
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-05 16:52:49 +0200

    setup js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// creo una lista per i numeri
+const ul = document.querySelector ("ul.list");
+const li = document.createElement ('li');
+
+// numeri da 1 a 100
+for (let i = 1; i <= 100; i++) {
+    const element = `<li class="box box--${i}">${i}</li>`;
+    ul.innerHTML += element;
+    // li.append (i);
+    ul.append (li);
+    console.log("numero: " + i);
+}
+
+// se ci sono multipli di 3 stampo Fizz
+// se ci sono multipli di 5 stampo Buzz
+// se ci sono sia multipli di 3 e che di 5 stampo FizzBuzz
+
+if ()
