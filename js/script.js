e00b57446997619b098248ace22745ef273164ba
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-06 14:52:36 +0200

    modifica operatore if

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 // creo una lista per i numeri
 const ul = document.querySelector ("ul.list");
-const li = document.createElement ('li');
+// const li = document.createElement ('li');
 
 // numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    const element = `<li class="box box--${i}">${i}</li>`;
-    ul.innerHTML += element;
-    // li.append (i);
-    ul.append (li);
+
     console.log("numero: " + i);
 
     /*
@@ -15,14 +12,22 @@ for (let i = 1; i <= 100; i++) {
       se ci sono multipli di 5 stampo Buzz
       se ci sono sia multipli di 3 e che di 5 stampo FizzBuzz
     */
-    if (i % 3 == 0) {
-        risultato = "fizz";
-    } else if (i % 5 == 0) {
-        risultato = "buzz";
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        const element = `<li class="box fizz-buzz">FizzBuzz</li>`;
+        ul.innerHTML += element;
+    } else if (i % 3 === 0) {
+        const element = `<li class="box fizz">Fizz</li>`;
+        ul.innerHTML += element;
+    } else if (i % 5 === 0)  {
+        const element = `<li class="box buzz">Buzz</li>`;
+        ul.innerHTML += element;
     } else {
-        risultato = (i % 3 % 5 == 0);
-        risultato = "fizzbuzz";
+        const element = `<li class="box number">${i}</li>`;
+        ul.innerHTML += element;
     }
-    console.log(risultato);
+
+    console.log(ul);
+
+    // document.getElementById("mio_id").innerHTML = ;
 }
 
